69883d3b3b863832f97fb258ad2ca5c56b0dcf91
upendra <upendrasahu1199@gmail.com>
2023-05-14 19:51:16 +0530

    Added : Check Employee is Present or Absent

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,7 +1,29 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWage {
+
+    String name;
+
+    public EmployeeWage(String name) {
+        this.name = name;
+    }
+
+    public boolean checkEmployeeIsPresentOrAbsent() {
+        Random random = new Random();
+        boolean a = random.nextBoolean();
+        if (a == true) {
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        EmployeeWage employee1 = new EmployeeWage("Rohan");
+        employee1.checkEmployeeIsPresentOrAbsent();
     }
 }
