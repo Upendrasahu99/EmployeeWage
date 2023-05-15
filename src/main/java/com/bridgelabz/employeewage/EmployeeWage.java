0b9da85250b96914357ddae368df613796c38722
upendra <upendrasahu1199@gmail.com>
2023-05-16 00:44:54 +0530

    Added : Using Switch Case Statement

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class EmployeeWage {
 
     String name;
-    boolean employeePresent;
+    int employeePresent;
     String employeeType;
     int wagePerHour;
     int totalDailyWage;
@@ -20,25 +20,30 @@ public class EmployeeWage {
 
     public void enterDailyWorkingHour() {
         if (employeeType == "FullTime") {
-           dailyWorkingHour = 8;
+            dailyWorkingHour = 8;
         } else if (employeeType == "PartTime") {
-           dailyWorkingHour = 4;
+            dailyWorkingHour = 4;
         }
     }
 
     public void checkEmployeeIsPresentOrAbsent() {
         Random random = new Random();
-        this.employeePresent = random.nextBoolean();
-        if (employeePresent == true) {
-            System.out.println("Employee is Present");
-        } else {
-            System.out.println("Employee is Absent");
+        this.employeePresent = random.nextInt(2);
+        switch (employeePresent) {
+            case 1:
+                System.out.println("Employee is Present");
+                break;
+            case 0:
+                System.out.println("Employee is Absent");
+                break;
+            default:
+
         }
     }
 
 
     public void calculatingDailyWage() {
-        if (employeePresent == true) {
+        if (employeePresent == 1) {
             this.totalDailyWage = dailyWorkingHour * wagePerHour;
             System.out.println(employeeType + " employee " + name + " total daily wage is " + totalDailyWage);
         }
