db9d55ec78e542b54b42bc6205184551754995ac
upendra <upendrasahu1199@gmail.com>
2023-05-17 01:16:04 +0530

    Added : Compute Employee Wage for multiple commpanies

--- src/main/java/com/bridgelabz/employeewage/RunEmployeeWageProgram.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.employeewage;
-
-public class RunEmployeeWageProgram {
-    public static void main(String[] args) {
-        EmployeeWage r = new EmployeeWage("ram", "FullTime", 20, 20, 100);
-        r.calculatingMonthlyWages();
-    }
-}
